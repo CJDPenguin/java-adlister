9b78fcc5848d2f6f9e2d1038b1359dc1da67a6a4
Chris Davis <christopher.j.davis03@gmail.com>
2022-07-20 10:52:07 -0500

    database & table creation

---
+++ src/main/adlister_migration.sql
@@ -0,0 +1,18 @@
+USE adlister_db;
+DROP TABLE IF EXISTS users;
+CREATE TABLE IF NOT EXISTS users (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    username VARCHAR(50) NOT NULL ,
+    email VARCHAR(100) NOT NULL,
+    password VARCHAR(50) NOT NULL,
+    PRIMARY KEY (id)
+);
+DROP TABLE IF EXISTS ads;
+CREATE TABLE IF NOT EXISTS ads (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    user_id INT NOT NULL,
+    title VARCHAR(200) NOT NULL,
+    description TEXT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (user_id) REFERENCES users (id)
+);
